9e8cbc6b8f860b8dd7e0180ea8752de9e81c849e
Alessandro Moreira <alessandro.bezerra@detran.ce.gov.br>
2019-06-28 07:30:26 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Button from '@material/react-button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import '@material/react-text-field/index.scss';
+import TextField, {HelperText, Input} from '@material/react-text-field';
+
+import '@material/react-material-icon/index.scss';
+import MaterialIcon from '@material/react-material-icon';
+
+
+import './App.scss';
+import "./react-button-overrides.scss";
+// add the appropriate line(s) in Step 3a if you are using compiled CSS instead.
+
+class App extends Component {
+  state = {value: 'Woof'};
+
+  render() {
+    return (
+      <div>
+        <br/>
+        <TextField
+          label='Dog'
+          helperText={<HelperText>Help Me!</HelperText>}
+          onTrailingIconSelect={() => this.setState({value: ''})}
+          //trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+          outlined='true'
+        ><Input
+           value={this.state.value}
+           onChange={(e) => this.setState({value: e.currentTarget.value})} />
+        </TextField>
+        
+        <Button
+          raised
+          className='button-alternate'
+          onClick={() => console.log('clicked!')}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Click Me!
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
