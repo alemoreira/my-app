000db3bf066929cfc7a19935081979ee0864e753
Alessandro Moreira <alessandro.bezerra@detran.ce.gov.br>
2019-06-28 07:56:09 -0300

    Updates

---
+++ precache-manifest.208dda0791e33966798e71aca6261f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197b6a9a2a02f9cfaf3a86f13899cfa8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9aa7afcbe46f3050a750",
+    "url": "/my-app/static/css/2.d48f3dd8.chunk.css"
+  },
+  {
+    "revision": "745819b8a8a26f19620f",
+    "url": "/my-app/static/css/main.193ef79d.chunk.css"
+  },
+  {
+    "revision": "9aa7afcbe46f3050a750",
+    "url": "/my-app/static/js/2.9289de69.chunk.js"
+  },
+  {
+    "revision": "745819b8a8a26f19620f",
+    "url": "/my-app/static/js/main.041aac2d.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
